cdfa5bd502ba46d3e4f89beb90c5a2c2ba2d0c1a
Danan Danurwenda <danandanurwendaadikusuma_1313621017@mhs.unj.ac.id>
2023-09-03 21:24:39 +0700

    tolong

--- __init__.py
+++ __init__.py
@@ -6,10 +6,11 @@ from IPython.display import display
 # def labelling():
 
 def process_data(data, header):
+    print(data)
     for head in header:
-        if head == 'width':
+        if head == 'leaf width':
             data[head] = data[head].astype(float)
-        elif head == 'length':
+        elif head == 'leaf length':
             data[head] = data[head].astype(float)
         elif head == 'species':
             data[head] = data[head].astype(str)
@@ -37,10 +38,20 @@ def main():
     pilih = menu()
     if pilih == 1:
         print("1.  Jika ada data baru, dengan kolom yang ada, bisa atau tidak menebak species data baru tersebut")
+
         data1 = dafa.read_csv('soal1.csv')
         header1 = data1.columns.tolist()
+
         res1 = process_data(data1, header1)
+        daun1 = Daon(data1['leaf width'], data1['leaf length'])
+
+        daun1.define_species()
+        res1 = res1._append(daun1, ignore_index=True)
+        # res1.append(daun1)
+
+        # print('Lebar daun : ' , daun1.width)
+        # print('Panjang daun : ' , daun1.length)
+        # print(type(res11))
+
         print(res1)
-        # res = res.append(res1, ignore_index=True)
-        # print(res)
 main()
